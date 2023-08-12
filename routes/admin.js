1e0060b3ccb02b932849e8ec9708299bf3b2b928
Mohammad Farman <farmankhanmlc@gmail.com>
2023-08-12 10:23:57 +0530

    add auth validation in every routes

--- routes/admin.js
+++ routes/admin.js
@@ -4,12 +4,12 @@ const checkAuth = require("./../middleware/token_verify");
 const Delete = require("./../middleware/delete");
 const router = express.Router();
 
-router.get("/", AdminController.list);
-router.post("/", AdminController.addUser);
+router.get("/", checkAuth, AdminController.list);
+router.post("/", checkAuth, AdminController.addUser);
 router.get("/getBasicinfo", checkAuth, AdminController.getBasicinfo);
 router.post("/login", AdminController.login);
-router.get("/:_id", AdminController.userDetail);
-router.put("/:_id", AdminController.editUser);
-router.delete("/:_id", Delete.deleteData, AdminController.editUser);
+router.get("/:_id", checkAuth, AdminController.userDetail);
+router.put("/:_id", checkAuth, AdminController.editUser);
+router.delete("/:_id", checkAuth, Delete.deleteData, AdminController.editUser);
 
 module.exports = router;
